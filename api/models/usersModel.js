8f00fb5aafe9afceac1ddaf01a818208224e21b1
Quentin JACQUET <qjacquet@smag-group.com>
2018-04-05 15:51:04 +0200

    Add password hash

--- api/models/usersModel.js
+++ api/models/usersModel.js
@@ -1,4 +1,8 @@
 let mongoose    = require('mongoose');
+let bcrypt      = require('bcrypt');
+
+const SALT_WORK_FACTOR = 10;
+
 let usersSchema = mongoose.Schema({
 
 	firstName: { type: String },
@@ -8,9 +12,31 @@ let usersSchema = mongoose.Schema({
 });
 
 //pre save the date for each entry
-usersSchema.pre('save', (next) => {
-	this.createdAt = new Date();
-	next();
+usersSchema.pre('save', function(next) {
+	var user = this;
+
+	// only hash the password if it has been modified (or is new)
+	if (!user.isModified('password')) return next();
+
+	// generate a salt
+	bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+		 if (err) return next(err);
+
+		 // hash the password using our new salt
+		 bcrypt.hash(user.password, salt, function(err, hash) {
+			  if (err) return next(err);
+
+			  // override the cleartext password with the hashed one
+			  user.password = hash;
+			  next();
+		 });
+	});
 });
+
+usersSchema.methods.comparePassword = function(candidatePassword, cb) {
+	bcrypt.compare(candidatePassword, this.password, function(err, res) {
+		 return cb(res);
+	});
+};
 
 module.exports = mongoose.model('Users', usersSchema);
